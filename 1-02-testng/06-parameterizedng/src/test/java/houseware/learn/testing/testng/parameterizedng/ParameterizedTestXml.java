a9792a28dbd9a3b24d4e4d30c0924ca5376afd4e
apasc <apascualco@gmail.com>
2016-06-05 20:45:22 +0200

    Parameterized test add

---
+++ 1-02-testng/06-parameterizedng/src/test/java/houseware/learn/testing/testng/parameterizedng/ParameterizedTestXml.java
@@ -0,0 +1,18 @@
+package houseware.learn.testing.testng.parameterizedng;
+
+import houseware.learn.testing.testng.parameterizedng.utils.Fibonacci;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+
+public class ParameterizedTestXml {
+
+
+    @Test
+    @Parameters({"number", "result"})
+    public void shorTArrayTest(int number, int result){
+        assertEquals(result, Fibonacci.fibonacci(number));
+    }
+}
