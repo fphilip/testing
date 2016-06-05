c5a6b56a39c07a7fce23e216a4fb81c4aee79ff4
apasc <apascualco@gmail.com>
2016-06-05 20:05:40 +0200

    Update testing

--- 1-01-junit/01-basic/src/test/java/houseware/learn/testing/junit/basic/BasicTest.java
+++ 1-01-junit/01-basic/src/test/java/houseware/learn/testing/junit/basic/BasicTest.java
@@ -1,9 +1,20 @@
 package houseware.learn.testing.junit.basic;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BasicTest {
 
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("@BeforeClass");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("@AfterClass");
+    }
     @Test
     public void basicTest(){
         //asserts
