fd26cf10595222cc340dec35413d7b9d4a7f05f7
apasc <apascualco@gmail.com>
2016-06-18 12:49:25 +0200

    complex selenium test

--- 3-05-selenium/complex/src/test/java/houseware/learn/testing/selenium/complex/ITestDropDownAjax.java
+++ 3-05-selenium/complex/src/test/java/houseware/learn/testing/selenium/complex/ITestDropDownAjax.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+
 public class ITestDropDownAjax extends AbstractSeleniumTest{
 
     @Test
