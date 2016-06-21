b0e9eec326a5a108c25273c4451e0699a7ab1503
Francisco Philip <fphilip@houseware.es>
2016-06-21 13:37:30 +0200

    ws selenium

--- 3-03-jws/src/main/java/houseware/learn/testing/WebServiceSample.java
+++
@@ -1,8 +0,0 @@
-package houseware.learn.testing;
-
-import javax.jws.WebService;
-
-@WebService
-public interface WebServiceSample {
-    String sayHello(String name);
-}
---
+++ 3-03-jws/src/test/java/houseware/learn/testing/jws/WebServiceSampleTest.java
@@ -0,0 +1,37 @@
+package houseware.learn.testing.jws;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.ws.Endpoint;
+import java.net.MalformedURLException;
+
+/**
+ * @author fphilip@houseware.es
+ */
+public class WebServiceSampleTest {
+    Endpoint endpoint ;
+   @Before
+    public void setUp() throws MalformedURLException {
+        System.setProperty("javax.xml.ws.spi.Provider", "com.sun.xml.internal.ws.spi.ProviderImpl");
+
+        WebServiceSampleImp e = new WebServiceSampleImp();
+         endpoint = Endpoint.publish("http://127.0.0.1:9190/8", e);
+//        Endpoint endpoint = new ProviderImpl().createAndPublishEndpoint("http://127.0.0.1:9876/8", e);
+//        endpoint.stop();
+
+
+    }
+    @After
+    public void tearDown(){
+        endpoint.stop();
+    }
+
+    @Test
+    public void test8() throws MalformedURLException {
+        WebServiceSample wsl = WebServiceSampleClient.client("http://127.0.0.1:9190/8?wsdl");
+        System.err.println("Say:"+wsl.say("Me"));
+    }
+
+}
