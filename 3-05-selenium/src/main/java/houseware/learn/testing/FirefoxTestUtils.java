c44495a3e48c4bb71ce78de7f12f3221d8e6dbaf
apasc <apascualco@gmail.com>
2016-09-04 15:55:08 +0200

    Add assumeThat for the browser driver

--- 3-05-selenium/src/main/java/houseware/learn/testing/FirefoxTestUtils.java
+++ 3-05-selenium/src/main/java/houseware/learn/testing/FirefoxTestUtils.java
@@ -1,21 +1,22 @@
 package houseware.learn.testing;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.MarionetteDriver;
 
-import java.io.IOException;
+import java.io.File;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assume.assumeThat;
 
 /**
  * @author fphilip@houseware.es
  */
 public class FirefoxTestUtils {
 
-    public static Process exec() throws IOException {
-        return Runtime.getRuntime().exec("/Users/fphilip/Downloads/geckodriver-0.8.0-OSX" /* " --connect-existing"*/);
-    }
-
+    //"/Users/fphilip/Downloads/geckodriver-0.8.0-OSX"
     public static void configure() {
-        System.setProperty("webdriver.gecko.driver", "/Users/fphilip/Downloads/geckodriver-0.8.0-OSX");
+        String firefoxPath = "C:/Users/apasc/Downloads/geckodriver.exe";
+        assumeThat(new File(firefoxPath).exists(), is(true));
+        System.setProperty("webdriver.gecko.driver", firefoxPath);
     }
 
     public static MarionetteDriver newDriver() {
